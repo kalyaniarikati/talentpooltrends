0ccca4741e30dce85917e61d5f1bcb40a4993d47
lkb-t440p <lkb-t440p@pop-os.localdomain>
2020-10-20 19:57:49 +1100

    synchronous readFile json to db

--- config/data.js
+++ config/data.js
@@ -4,11 +4,90 @@ const IndeedUk = require('../model/IndeedUk');
 const IndeedUsa = require('../model/IndeedUsa');
 const SeekAus = require('../model/SeekAus');
 
-const fsPromises = require('fs').promises
+// const fsPromises = require('fs').promises
 const fs = require('fs')
 
 async function loadData() {
 
+    // synchronous version
+    // seekAus
+    let seekAusData = fs.readFileSync('./data/seek_aus.json', 'utf-8')
+    let parsedSeekAus= JSON.parse(seekAusData)
+        console.log(parsedSeekAus)
+        parsedSeekAus.jobs.forEach(job => {
+            let seekAus = new SeekAus(
+                {
+                title: job.title,
+                company: job.company,
+                location: job.location,
+                url: job.url,
+                blurb: job.blurb
+            });
+            console.log(seekAus)
+            seekAus.save(function(err) {
+                if (err) return err;
+            })
+        })
+
+    // indeedAus
+    let indeedAusData = fs.readFileSync('./data/indeed_aus.json', 'utf-8')
+    let parsedIndeedAus = JSON.parse(indeedAusData)
+        console.log(parsedIndeedAus)
+        parsedIndeedAus.jobs.forEach(job => {
+            let indeedAus = new IndeedAus(
+                {
+                title: job.title,
+                company: job.company,
+                location: job.location,
+                url: job.url,
+                blurb: job.blurb
+            });
+            console.log(indeedAus)
+            indeedAus.save(function(err) {
+                if (err) return err;
+            })
+        })
+
+    // indeedUk
+    let indeedUkData = fs.readFileSync('./data/indeed_uk.json', 'utf-8')
+    let parsedIndeedUk = JSON.parse(indeedUkData)
+        console.log(parsedIndeedUk)
+        parsedIndeedUk.jobs.forEach(job => {
+            let indeedUk = new IndeedUk(
+                {
+                title: job.title,
+                company: job.company,
+                location: job.location,
+                url: job.url,
+                blurb: job.blurb
+            });
+            console.log(indeedUk)
+            indeedUk.save(function(err) {
+                if (err) return err;
+            })
+        })
+
+    // indeedUsa
+    let indeedUsaData = fs.readFileSync('./data/indeed_usa.json', 'utf-8')
+    let parsedIndeedUsa = JSON.parse(indeedUsaData)
+        console.log(parsedIndeedUsa)
+        parsedIndeedUsa.jobs.forEach(job => {
+            let indeedUsa = new IndeedUsa(
+                {
+                title: job.title,
+                company: job.company,
+                location: job.location,
+                url: job.url,
+                blurb: job.blurb
+            });
+            console.log(indeedUsa)
+            indeedUsa.save(function(err) {
+                if (err) return err;
+            })
+        })
+    }
+
+    //Async version
     // var parsedIndeedAus;
     // let indeedAusData = await fsPromises.readFile('./data/indeed_aus.json', 'utf-8', (err, data) => {
     //     if (err) {
@@ -33,7 +112,6 @@ async function loadData() {
     //     })
     // });
     
-    // async version
     // var parsedIndeedUk;
     // let indeedUkData = await fsPromises.readFile('./data/indeed_uk.json', 'utf-8', (err, data) => {
     //     if (err) {
@@ -58,24 +136,6 @@ async function loadData() {
     //     })
     // });
     
-    // synchronous version
-    let seekAusData = fs.readFileSync('./data/seek_aus.json', 'utf-8')
-    let parsedSeekAusData = JSON.parse(seekAusData)
-        console.log(parsedSeekAusData)
-        parsedSeekAusData.jobs.forEach(job => {
-            let seekAus = new SeekAus(
-                {
-                title: job.title,
-                company: job.company,
-                location: job.location,
-                url: job.url,
-                blurb: job.blurb
-            });
-            console.log(seekAus)
-            seekAus.save(function(err) {
-                if (err) return err;
-            })
-        })
     
     // let indeedUsaData = await fsPromises.readFile('./data/indeed_usa.json', 'utf-8', (err, data) => {
     //     if (err) {
@@ -154,6 +214,6 @@ async function loadData() {
     //     })
     // })
 
-}
+
 
 module.exports = { loadData }
