ee426333c8b6481f0b3c08b5f841fb34652d23d9
kalyaniarikati <kalyani.arikati@gmail.com>
2020-12-07 12:52:19 +1100

    keys ignore

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports={
-    MongoURI: 'mongodb+srv://ka:kalyani1209@cluster0.ks8lx.mongodb.net/blog_app?retryWrites=true&w=majority',
-    APP_ID: '85ff9cc6',
-    API_KEY: '805ee11c8ab36004576ccc32254a55af',
-    BASE_URL: 'https://api.adzuna.com/v1/api/jobs',
-    BASE_PARAMS: 'search/1?&results_per_page=20&content-type=application/json'
-}
